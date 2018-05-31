a37c4a58f1c53612b2ca1000cfaa51453efb0f5c
Horizon-42 <liudxp@live.cn>
2018-05-31 08:35:26 +0800

    计算逆波兰式

--- RP/ReversePolish.py
+++ RP/ReversePolish.py
@@ -1,13 +1,11 @@
 from stack import Stack
 
-priority = {'*': 2, '/': 2, '+': 1, '-': 1, '#': 0}
 
-
-def middle_to_rp():
-    global priority
+def middle_to_rp(inputfile):
+    priority = {'(': 3, ')': 3, '*': 2, '/': 2, '+': 1, '-': 1, '~': 1, '#': 0}
     rps = []
     try:
-        input = open('RP.in', 'r', encoding='utf-8')
+        input = open(inputfile, 'r', encoding='utf-8')
         for line in input:
             line = line.strip('\n')
             line = line+'#'
@@ -17,19 +15,27 @@ def middle_to_rp():
             rp = ""
             pre = ''
             for char in line:
+                # 预处理 处理正号、负号
+                if char == '+':
+                    if pre in ['+', '-', '(']:
+                        continue
+                elif char == '-':
+                    if pre in ['+', '-', '(']:
+                        char = '~'
+                # 求逆波兰式
                 if char == '(':
                     ops.push(char)
                 elif char == ')':
                     while ops.peek() != '(':
                         if ops.peek() == '#':
-                            raise ValueError(char)
+                            raise ValueError('( ) must be couple.')
                         rp += ops.pop()
                     ops.pop()
                 elif char == '#':
                     while ops.peek() != '#':
                         rp += ops.pop()
                         if rp[-1] == '(':
-                            raise ValueError(char)
+                            raise ValueError('( ) must be couple.')
                 elif char in priority:
                     if priority[char] > priority[ops.peek()]:
                         ops.push(char)
@@ -39,25 +45,63 @@ def middle_to_rp():
                         ops.push(char)
                 else:
                     rp += char
+                pre = char
+
             rps.append(rp)
-    except IOError as error:
-        print("Failed to open file, error: %s" % error)
+    except IOError as ioerror:
+        print("Failed to open file, error: %s" % ioerror)
+    except ValueError as verror:
+        print("Input error! %s" % verror)
+
     return rps
 
 
-def compute():
+def init_var(inputfile):
     values = {}
     try:
-        input = open('Value.in', 'r', encoding='utf-8')
+        input = open(inputfile, 'r', encoding='utf-8')
+        for line in input:
+            line = line.strip('\n')
+            mors = line.split(';')
+            for mor in mors:
+                [var, value] = mor.split('=')
+                if not value.isdigit():
+                    raise ValueError("%s should be digit.")
+                if var in values:
+                    values[var] = float(value)
+                else:
+                    values.update({var: float(value)})
+    except IOError as ioerror:
+        print("Failed to open file, error: %s" % ioerror)
+    except ValueError as verror:
+        print("Input Error: %s" % verror)
 
-    except IOError as error:
-        print("Failed to open file, error: %s" % error)
+    return values
+
+
+def compute_rp(rp, values):
+    op1 = {'~': lambda x: -x}
+    op2 = {'+': lambda y, x: x + y, '-': lambda y, x: x - y,
+           '/': lambda y, x: x/y, '*': lambda y, x: x*y}
+    res = Stack()
+    for char in rp:
+        if char in op1:
+            assert not res.is_empty()
+            res.push(op1[char](res.pop()))
+        elif char in op2:
+            assert not res.is_empty()
+            res.push(op2[char](res.pop(), res.pop()))
+        else:
+            res.push(values[char])
+    assert res.size() == 1
+    return res.pop()
 
 
 if __name__ == '__main__':
-    try:
-        rps = middle_to_rp()
-        for rp in rps:
-            print(rp)
-    except ValueError as error:
-        print("Input error! %s" % error)
+    rps = middle_to_rp('RP.in')
+    for rp in rps:
+        print(rp)
+    values = init_var('Values.in')
+    print(values)
+
+    print(compute_rp(rp, values))
